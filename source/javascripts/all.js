b25f2862dadc218f9e202694351fe809f90ff9d3
Gary A. Newsome <garyanewsome@gmail.com>
2016-09-19 15:04:14 -0400

    Code review fixes

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
     var showChar = 250;  // How many characters are shown by default
     var ellipsestext = "...";
     var moretext = "Click to learn more.";
-    // var lesstext = "Show less";
 
 
     $('.more').each(function() {
@@ -17,28 +16,8 @@ $(document).ready(function() {
             var c = content.substr(0, showChar);
             var h = content.substr(showChar, content.length - showChar);
 
-            // var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
             var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a class="morelink">' + moretext + '</a></span>';
             $(this).html(html);
         }
     });
-
-    // $(".morelink").click(function(){
-        
-        // if($(this).hasClass("less")) {
-        //     $(this).removeClass("less");
-        //     $(this).html(moretext);
-        // } else {
-        //     $(this).addClass("less");
-        //     $(this).html(lesstext);
-        // }
-        // $(this).parent().prev().toggle(400);
-        // $(this).prev().toggle();
-        // return false;
-    // });
 });
-
-
-
-
-
