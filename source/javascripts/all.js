29a739cae0ecd7a7dbe17bb77bfb4a4d14f6afb8
Gary A. Newsome <garyanewsome@gmail.com>
2016-09-19 13:32:07 -0400

    new team page established

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -5,8 +5,8 @@ $(document).ready(function() {
     // Configure/customize these variables.
     var showChar = 250;  // How many characters are shown by default
     var ellipsestext = "...";
-    var moretext = "Show more >";
-    var lesstext = "Show less";
+    var moretext = "Click to learn more.";
+    // var lesstext = "Show less";
 
 
     $('.more').each(function() {
@@ -17,37 +17,28 @@ $(document).ready(function() {
             var c = content.substr(0, showChar);
             var h = content.substr(showChar, content.length - showChar);
 
-            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
-
+            // var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a class="morelink">' + moretext + '</a></span>';
             $(this).html(html);
         }
     });
 
-    $(".morelink").click(function(){
-        if($(this).hasClass("less")) {
-            $(this).removeClass("less");
-            $(this).html(moretext);
-        } else {
-            $(this).addClass("less");
-            $(this).html(lesstext);
-        }
-        $(this).parent().prev().toggle(400);
-        $(this).prev().toggle();
-        return false;
-    });
-
-
-    $('.alex-button').click(function(){
-        $(this).find('.alex-content').clone().appendTo('.alex-modal-body');
-        $('#alex-modal .alex-modal-body .control-group.hide').show();
-        $('#alex-modal').modal();
-
-    });
-    
-    $('#alex-modal').on('hidden', function(){
-        $('#alex-modal .alex-content').remove();
-    });
-
+    // $(".morelink").click(function(){
+        
+        // if($(this).hasClass("less")) {
+        //     $(this).removeClass("less");
+        //     $(this).html(moretext);
+        // } else {
+        //     $(this).addClass("less");
+        //     $(this).html(lesstext);
+        // }
+        // $(this).parent().prev().toggle(400);
+        // $(this).prev().toggle();
+        // return false;
+    // });
 });
+
+
+
 
 
