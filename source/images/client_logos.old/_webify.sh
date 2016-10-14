bc1f2bf59ca54633e1a96aec6f1c7ba44d57a077
Gary A. Newsome <garyanewsome@gmail.com>
2016-10-14 14:17:25 -0400

    Remove `.old` logo bar images

--- source/images/client_logos.old/_webify.sh
+++
@@ -1,9 +0,0 @@
-# Use a simple shell loop, to process each of the images.
-mkdir web
-find * -prune -name '*.jpg' \
-       -exec  convert '{}' -resize x120 web/web_'{}' \;
-
-# Use find to substitute filenames into a 'convert' command.
-# This also provides the ability to recurse though directories by removing
-# the -prune option, as well as doing other file checks (like image type,
-# or the disk space used by an image).
