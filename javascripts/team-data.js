2a3c25c8cea798034175a30aa339773b8323347a
Alex Loya <aloys529@gmail.com>
2018-02-28 14:39:50 -0500

    2018 web

--- javascripts/team-data.js
+++
@@ -1,2 +0,0 @@
-window.team = {"joe-thoresen":"001","carl-thoresen-ph-d":"002","denise-meyer-kennell-m-s":"003","jackie-hodges":"004","alex-loya":"005","kim-m-catania":"006","david-a-jencen-ph-d":"007","paul-scarrone":"008","eric-thoresen":"009","joyce-thoresen":"010","becky-watts":"011"}
-;
